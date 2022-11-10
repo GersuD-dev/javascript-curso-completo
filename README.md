25d2d50767a98f877bf6a09885fbc76822100643
Gerson Durães <42494933+GersuD-dev@users.noreply.github.com>
2022-11-10 02:42:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# javascript-curso-completo
+ Repositório para atividades do curso completo de JavaScript com o professor Gustavo Guanabara do Curso em Vídeo.
---
+++ site-fundo-horario-cliente/js/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var horarioatual = document.getElementById('hora');
+    var imagem = document.getElementById('imagem');
+
+    var data = new Date();
+    var hora = data.getHours();
+    horarioatual.innerHTML = `Agora são ${hora} hora(s)`;
+
+    if(hora >= 6 && hora < 12){
+        // bom dia
+        imagem.src = 'img/manha.png';
+        document.body.style.background = '#7f99a0';
+    }else if(hora >= 12 && hora < 18){
+        //boa tarde
+        imagem.src = 'img/tarde.png'
+        document.body.style.background = '#c55403';
+    }else{
+        //boa noite
+        imagem.src = 'img/noite.png'
+        document.body.style.background = '#033752';
+    }
+}
